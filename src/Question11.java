a9852eed1920ea9e8f9aabdf7a0f7368cca3c0a0
rahulRajput2005 <rahul2005rajput09@gmail.com>
2026-04-06 16:43:18 +0530

    Update Question11.java Removed the extra print statements

--- src/Question11.java
+++ src/Question11.java
@@ -4,29 +4,27 @@ public class Question11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //Calculate electricity Bill
         System.out.print("Enter units to calculate electricity bill: ");
         int units = sc.nextInt();
 
-        double bill;
+        float bill = 0;
 
-        if(units >= 0 && units <= 200){
-            System.out.println("charges : 0");
-        }else if(units > 200 && units <= 300){
-            bill = ( units - 200 ) * 1.2;
-            System.out.println("Charges: " + bill);
-        }else if(units > 300 && units <= 400){
-            bill = 100*1.2 + (units-300)*1.5;
-            System.out.println("Charges: " + bill);
-        }else if(units > 400 && units <= 500){
-            bill = 100*1.2 + 100*1.5 + (units-400)*2.5;
-            System.out.println("Charges: " + bill);
-        }else if(units > 500){
-            bill = 100*1.2 + 100*1.5 + 100*2.5 + (units-500)*8;
-            System.out.println("Charges: " + bill);
-        }else{
+        if (units >= 0 && units <= 200)
+            bill = 0;
+        else if (units <= 300)
+            bill = (units - 200) * 1.2f;
+        else if (units <= 400)
+            bill = 100 * 1.2f + (units - 300) * 1.5f;
+        else if (units <= 500)
+            bill = 100 * 1.2f + 100 * 1.5f + (units - 400) * 2.5f;
+        else if (units > 500)
+            bill = 100 * 1.2f + 100 * 1.5f + 100 * 2.5f + (units - 500) * 8f;
+        else {
             System.out.println("Invalid Units");
+            return;
         }
+
+        System.out.println("Charges: " + bill);
 
 
     }
